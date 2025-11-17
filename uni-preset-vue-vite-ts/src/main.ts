83b1188d35b96c83717efce6e53c5cd3f13fa0d1
moshiqiqian <bb313317725@gmail.com>
2025-11-17 21:25:51 +0800

    更新

--- uni-preset-vue-vite-ts/src/main.ts
+++ uni-preset-vue-vite-ts/src/main.ts
@@ -1,7 +1,17 @@
 import { createSSRApp } from "vue";
+import { createPinia } from 'pinia'; // 1. 导入 createPinia
 import App from "./App.vue";
+
 export function createApp() {
+  // 使用 createSSRApp 创建应用实例（uni-app 推荐用于跨端渲染）
   const app = createSSRApp(App);
+  
+  // 2. 创建 Pinia 实例
+  const pinia = createPinia();
+  
+  // 3. 将 Pinia 挂载到 Vue 应用上
+  app.use(pinia);
+  
   return {
     app,
   };
